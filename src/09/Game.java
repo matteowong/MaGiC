f573c58a6c4760f1654a0d0dd0c3de4ff28a1cf2
csmithsalzberg <caleb.smithsalzberg@gmail.com>
2017-01-09 22:00:47 -0500

    translator

--- src/09/Game.java
+++ src/09/Game.java
@@ -1,11 +1,38 @@
-public class Game {
-    private Player _guesser;
-    private Player _masterMind;
-    private int[][] _board = int[12][4];
-    private int[][] _pegs=int[12][4];
-    private int _turns;
-    private int[] _correctAns=int[4];
+import java.util.ArrayList;
 
+public class Game {
+    //private Player _guesser; //commented to avoid error for time being
+    //private Player _masterMind; //see above
+    private int[][] _board = new int[12][4];
+    private int[][] _pegs= new int[12][4];
+    private int _turns;
+    private int[] _correctAns= new int[4];
+    private static String[][] matching = {{"1","blue"},{"2","red"},{"3","orange"},{"4","yellow"},{"5","purple"},{"6","brown"}};
+    
+    
+    public static String searchColor(String key){
+        for (int i=0; i<6; i++){
+            if (matching[i][1].equals(key))
+                return matching[i][0];
+        }
+        return "";
+    }
+    public static ArrayList<String> translatorToNum(ArrayList<String> input){
+        ArrayList<String> output = new ArrayList<String>();
+        for (int i=0; i<input.size(); i++){
+            output.add(searchColor(input.get(i)));
+        }
+        return output; //NOTE: output is a string, so must still parse to get int
+    }
+    //testing
+    public static void main(String[] args){
+        String[] x = new String[] {"blue","brown","orange","purple","blue","yellow"};
+        ArrayList<String> t = new ArrayList<String>();
+        for (String ret:x)
+		  t.add(ret);
+        Game test=new Game();
+        System.out.println(test.translatorToNum(t)); //expect 1 6 3 5 1 4
+    }
 
 
 }
