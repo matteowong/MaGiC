8377ef99c1bc21fe6648d419815ae6f54a8f9448
Matteo Wong <Matteo@Matteos-MacBook-Air.local>
2017-01-10 16:43:11 -0500

    User constructor and setGuess method

--- src/10/User.java
+++ src/10/User.java
@@ -1,4 +1,12 @@
 public class User extends Player {
+
+    public Player() {
+	_masterOrGuesser=true;
+	_gameBoard=new int[12][4];
+	_pegsBoard=new int[12][4];
+	_currentTurn=0;
+    }
+    
     public int[] setGuess(int[] guess) {
 	_currGuess=guess;
 	gameBoard[_currentTurn]=guess;
