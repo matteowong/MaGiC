b34d30d31c94a1002c97965d04a1e1f3e147556c
Matteo Wong <Matteo@Matteos-MacBook-Air.local>
2017-01-09 22:17:54 -0500

    tentative constructor in Game.java

--- src/09/Game.java
+++ src/09/Game.java
@@ -8,15 +8,29 @@ public class Game {
     private int _turns;
     private int[] _correctAns= new int[4];
     private static String[][] matching = {{"1","blue"},{"2","red"},{"3","orange"},{"4","yellow"},{"5","purple"},{"6","brown"}};
+
+    /*commented so no errors -- does not compile yet bc no User.java or Computer.java have been written
+	public Game(boolean userGuesser) {
+	if (userGuesser) {
+	    _guesser = new User();
+	    _masterMind = new Computer();
+	    _correctAns=_masterMind.setFinal();
+	}
+	else {
+	    _guesser=new Computer();
+	    _masterMind=new User();
+	}
+	_turns=0;
+    }*/
+
     
-    
-    public static String searchColor(String key){
-        for (int i=0; i<6; i++){
-            if (matching[i][1].equals(key))
-                return matching[i][0];
-        }
-        return "";
-    }
+	public static String searchColor(String key){
+	    for (int i=0; i<6; i++){
+		if (matching[i][1].equals(key))
+		    return matching[i][0];
+	    }
+	    return "";
+	}
     public static ArrayList<String> translatorToNum(ArrayList<String> input){
         ArrayList<String> output = new ArrayList<String>();
         for (int i=0; i<input.size(); i++){
@@ -29,7 +43,7 @@ public class Game {
         String[] x = new String[] {"blue","brown","orange","purple","blue","yellow"};
         ArrayList<String> t = new ArrayList<String>();
         for (String ret:x)
-		  t.add(ret);
+	    t.add(ret);
         Game test=new Game();
         System.out.println(test.translatorToNum(t)); //expect 1 6 3 5 1 4
     }
