a2255db83a8941e068bdf8158c799a935944422e
csmithsalzberg <caleb.smithsalzberg@gmail.com>
2017-01-10 21:05:17 -0500

    computer takes input guess, returns correct pegs

--- src/10/Computer.java
+++ src/10/Computer.java
@@ -1,10 +1,15 @@
-public class Computer extends player{
+public class Computer extends Player{
     public int[] makeAnswer(){
-	int[] answer= new int[6];
-	for (int i=0; i<6; i++){
-	    answer[i]=(int)Math.random()*6;
-	}
-	return answer;
+	   int[] answer= new int[6];
+	   for (int i=0; i<6; i++){
+           answer[i]=(int)Math.random()*6;
+	   }
+	   return answer;
     }
+    public static void main(String[] args){
+        Computer t = new Computer();
+        System.out.println(t.givePegs());   
+    }
+    
 	    
 }
