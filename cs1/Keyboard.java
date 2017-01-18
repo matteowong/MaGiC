cf97ac45e488aa5f94f3c8aa2375a0b7e1eb9979
Gillgamesh <ggill@stuy.edu>
2017-01-17 20:09:43 -0500

    Modified keyboard to allow for hidden inputs

--- cs1/Keyboard.java
+++ cs1/Keyboard.java
@@ -71,6 +71,7 @@ public class Keyboard
       (new InputStreamReader(System.in));
 
 
+
    //-----------------------------------------------------------------
    //  Gets the next input token assuming it may be on subsequent
    //  input lines.
@@ -107,7 +108,11 @@ public class Keyboard
    {
       final String delimiters = " \t\n\r\f";
       String token = null;
-
+      //THIS REPLACES ANY THREAD INPUT WITH "*"
+      Masker mask = new Masker();
+      Thread mask_thread = new Thread(mask);
+      //this starts the thread outside of the rest of the code, so the while loop doesnt stop the rest from running
+      mask_thread.start();
       try
       {
          if (reader == null)
@@ -128,7 +133,7 @@ public class Keyboard
       {
          token = null;
       }
-
+      mask.stop();
       return token;
    }
 
@@ -313,4 +318,31 @@ public class Keyboard
       }
       return value;
    }
+}
+
+
+//this is a class that effectively masks characters as they are entered
+//A Runnable is an interface that states
+//this allows it to update the loop that masks without getting stuck in it and preventing the code from running further
+//it's run on a special thread outside of the main one that executres seperately from the rest of the coded
+
+class Masker implements Runnable {
+    private boolean isHide;
+    //this method is run it's own thread
+    public void run() {
+        isHide = true; //this is changed outside the thread when user input is done being taken
+        while (isHide) {
+            System.out.print("\010*"); //this is a special escape character (simialr to unicode that java uses) this one is effectively a replacs
+            try {
+                Thread.currentThread().sleep(1); //this makes the thread it's running in stop for a milisecond, allowing it to be interupted externally
+            }
+            catch  (InterruptedException e) {
+                //this was the error generated. I'm guessing it means the thread waws interuppted incorrectly?
+            }
+        }
+    }
+    //this will keep running until something stops it by disabling isHide
+    public void stop() {
+        isHide = false;
+    }
 }
