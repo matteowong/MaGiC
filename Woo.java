afb0d33b662c656d6643168219695bcfcd06c92d
Caleb Smith-Salzberg <csmithsalzberg@stuy.edu>
2017-01-22 16:29:29 -0500

    Update Woo.java

--- Woo.java
+++ Woo.java
@@ -5,6 +5,7 @@ public class Woo {
 	System.out.println("Welcome to MaGiC's MasterMind. In this game you are trying to guess a code produced by the MasterMind. The code is any combination of the digits 1-6, of length 4, with repeats allowed. Example codes are: 1633, 1425, 5555\n");
 	System.out.println("Input guesses in this format: xxxx. Example: 1234.\n");
 	System.out.println("The guesser  will receive 'pegs' in response. These pegs tell the guesser how close their guess is and will be in the format xxxx. A 1 corresponds to the right number in the wrong place, a 2 means the right number in the right place, and a 0 means it is the wrong number. For example, if the inputted guess is 3434 and the correct code is 5364, then the pegs returned should be 2100. Remember, order does not matter (2020 is the same as 2200 etc.). Example pegs: 2100, 1110, 2222 (the guesser has won in this case). Enjoy!");
+	System.out.println("If all the pegs are 2s, then every peg is the correct color, in the correct position, and the guesser has won");
 	System.out.println();
 	System.out.println("Decide how you want to play:");
 	System.out.println("1 if you want to be the guesser");
